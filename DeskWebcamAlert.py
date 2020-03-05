dfb0d5248bf4d178181c9f64ef54b7af9700ecf2
Nick Krenowicz <nkrenowicz@uidahol.edu>
2020-03-04 18:52:32 -0800

    New code base

--- DeskWebcamAlert.py
+++ DeskWebcamAlert.py
@@ -1,64 +1,134 @@
 import tkinter as tk
-import time
 import cv2
-import PIL.Image, PIL.ImageTK
+import PIL.Image, PIL.ImageTk
+import time
+import datetime as dt
+import argparse
 
 # Reference: https://solarianprogrammer.com/2018/04/21/python-opencv-show-video-tkinter-window/
 # Reference2: https://www.pyimagesearch.com/2016/05/30/displaying-a-video-feed-with-opencv-and-tkinter/
 # TK doc: https://docs.python.org/3/library/tk.html
 # TK guide: https://likegeeks.com/python-gui-examples-tkinter-tutorial/
+# Source: https://cppsecrets.com/users/5779711011711497103106971051104854494864103109971051084699111109/Python-GUI-application-to-record-a-video-using-Tkinter-GUI-module-and-openCV-computer-vision-library.php
+
 
 class App:
     def __init__(self, window, window_title, video_source=0):
         self.window = window
         self.window.title(window_title)
         self.video_source = video_source
-        #self.window.geometry('350x200')
-        self.btn = tk.Button(self.window, text="Hide", command=self.hide)
-        #self.btn.grid(column=0, row=1)
+        self.ok=False
 
-        # open video source
-        self.vid = MyVideoCapture(video_source)
+        # open video source (by default this will try to open the computer webcam)
+        self.vid = VideoCapture(self.video_source)
+
+        # Create a canvas that can fit the above video source size
         self.canvas = tk.Canvas(window, width = self.vid.width, height = self.vid.height)
         self.canvas.pack()
 
-        # After it is called once, the update method will be automatically called every delay 
-        self.delay = 100
+        # Button that lets the user take a snapshot
+        self.btn_snapshot=tk.Button(window, text="Snapshot", command=self.snapshot)
+        self.btn_snapshot.pack(side=tk.LEFT)
+
+        #video control buttons
+
+        #self.btn_start=tk.Button(window, text='START', command=self.open_camera)
+        #self.btn_start.pack(side=tk.LEFT)
+
+        #self.btn_stop=tk.Button(window, text='STOP', command=self.close_camera)
+        #self.btn_stop.pack(side=tk.LEFT)
+
+        # quit button
+        self.btn_quit=tk.Button(window, text='QUIT', command=quit)
+        self.btn_quit.pack(side=tk.LEFT)
+
+        # After it is called once, the update method will be automatically called every delay milliseconds
+        self.delay=10
         self.update()
-        
+
         self.window.mainloop()
-    
-    def update(self):
-        # Do stuff
-        print ("Hi")
-        ret, frame = self.vid.get_frame()
+
+    def snapshot(self):
+        # Get a frame from the video source
+        ret,frame=self.vid.get_frame()
+
         if ret:
-            self.photo = PIL.ImageTK.Photo.Image(imag = PIL.Imag.fromarray(frame))
-            self.canvas.create_image(0,0,image = self.photo, anchor = tk.NW)
-        self.window.after(self.delay, self.update)
+            cv2.imwrite("frame-"+time.strftime("%d-%m-%Y-%H-%M-%S")+".jpg",cv2.cvtColor(frame,cv2.COLOR_RGB2BGR))
 
-    def hide(self):
-        self.window.iconify()
-        time.sleep(3)
-        self.window.deiconify()
+    def open_camera(self):
+        self.ok = True
+        #self.timer.start()
+        print("camera opened => Recording")
 
-class MyVideoCapture:
+
+
+    def close_camera(self):
+        self.ok = False
+        #self.timer.stop()
+        print("camera closed => Not Recording")
+
+       
+    def update(self):
+
+        # Get a frame from the video source
+        ret, frame = self.vid.get_frame()
+        if self.ok:
+            self.vid.out.write(cv2.cvtColor(frame,cv2.COLOR_RGB2BGR))
+
+        if ret:
+            self.photo = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(frame))
+            self.canvas.create_image(0, 0, image = self.photo, anchor = tk.NW)
+        self.window.after(self.delay,self.update)
+
+
+class VideoCapture:
     def __init__(self, video_source=0):
         # Open the video source
         self.vid = cv2.VideoCapture(video_source)
         if not self.vid.isOpened():
             raise ValueError("Unable to open video source", video_source)
 
-        # Get video source width and height
-        self.width = self.vid.get(cv2.CAP_PROP_FRAME_WIDTH)
-        self.height = self.vid.get(cv2.CAP_PROP_FRAME_HEIGHT)
+        # Command Line Parser
+        args=CommandLineParser().args
 
+        
+        #create videowriter
+
+        # 1. Video Type
+        VIDEO_TYPE = {
+            'avi': cv2.VideoWriter_fourcc(*'XVID'),
+            #'mp4': cv2.VideoWriter_fourcc(*'H264'),
+            'mp4': cv2.VideoWriter_fourcc(*'XVID'),
+        }
+
+        self.fourcc=VIDEO_TYPE[args.type[0]]
+
+        # 2. Video Dimension
+        STD_DIMENSIONS =  {
+            '480p': (640, 480),
+            '720p': (1280, 720),
+            '1080p': (1920, 1080),
+            '4k': (3840, 2160),
+        }
+        res=STD_DIMENSIONS[args.res[0]]
+        print(args.name,self.fourcc,res)
+        self.out = cv2.VideoWriter(args.name[0]+'.'+args.type[0],self.fourcc,10,res)
+
+        #set video sourec width and height
+        self.vid.set(3,res[0])
+        self.vid.set(4,res[1])
+
+        # Get video source width and height
+        self.width,self.height=res
+
+
+    # To get frames
     def get_frame(self):
         if self.vid.isOpened():
             ret, frame = self.vid.read()
             if ret:
                 # Return a boolean success flag and the current frame converted to BGR
-                return (ret, cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+                return (ret, cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))
             else:
                 return (ret, None)
         else:
@@ -68,9 +138,38 @@ class MyVideoCapture:
     def __del__(self):
         if self.vid.isOpened():
             self.vid.release()
-        self.window.mainloop()
+            self.out.release()
+            cv2.destroyAllWindows()
+
+
+class CommandLineParser:
+    
+    def __init__(self):
+
+        # Create object of the Argument Parser
+        parser=argparse.ArgumentParser(description='Script to record videos')
+
+        # Create a group for requirement 
+        # for now no required arguments 
+        # required_arguments=parser.add_argument_group('Required command line arguments')
+
+        # Only values is supporting for the tag --type. So nargs will be '1' to get
+        parser.add_argument('--type', nargs=1, default=['avi'], type=str, help='Type of the video output: for now we have only AVI & MP4')
+
+        # Only one values are going to accept for the tag --res. So nargs will be '1'
+        parser.add_argument('--res', nargs=1, default=['480p'], type=str, help='Resolution of the video output: for now we have 480p, 720p, 1080p & 4k')
+
+        # Only one values are going to accept for the tag --name. So nargs will be '1'
+        parser.add_argument('--name', nargs=1, default=['output'], type=str, help='Enter Output video title/name')
+
+        # Parse the arguments and get all the values in the form of namespace.
+        # Here args is of namespace and values will be accessed through tag names
+        self.args = parser.parse_args()
 
 
 
+def main():
+    # Create a window and pass it to the Application object
+    App(tk.Tk(),'Video Recorder')
 
-a = App(tk.Tk(), "CV testing")
+main()
